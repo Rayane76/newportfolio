1ef8e9736aac67677d5e0ffbf4d4cb8174f0ab3e
Rayane <rayanehiouani.rh@gmail.com>
2024-08-18 20:06:29 +0100

    cvb

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,38 @@ import PersonalProjects from "./components/home/personalProjects/PersonalProject
 import Projects from "./components/home/projects/Projects";
 import Skills from "./components/home/skills/Skills";
 import Work from "./components/home/work/Work";
+import { Resend } from 'resend';
+import { headers } from 'next/headers'
+import EmailTemplate from "./components/EmailTemplate";
 
-export default function Home() {
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+
+
+export default async function Home() {
+  
+  const header = headers()
+  const ip = (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
+
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Rayane Portfolio <info@rayanehiouani.me>',
+      to: ['rayanehiouani.rh@gmail.com'],
+      subject: 'New Visitor !',
+      react: EmailTemplate({ ip }),
+    });
+
+    if (error) {
+      console.log(error);
+    }
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <>
          <Hero />
