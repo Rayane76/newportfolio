9d8deea36e19b33c4f146ba3e7d6bdaf1d682133
Rayane <rayanehiouani.rh@gmail.com>
2025-05-23 16:02:06 +0100

    new experience

--- src/app/components/home/projects/Projects.jsx
+++ src/app/components/home/projects/Projects.jsx
@@ -4,40 +4,51 @@ import Card from "../Card";
 function Projects() {
   return (
     <div id="projects" className="flex justify-center w-full">
-    <div style={{maxWidth:"1200px",width:"1200px"}} className="flex flex-col text-left justify-between pt-8 relative">
-      <div>
-        <SectionTitle title="Real-World Projects." />
-      </div>
-      <div className="grid grid-cols-1 gap-12 md:gap-5 md:grid-cols-3 items-start mt-5">
+      <div
+        style={{ maxWidth: "1200px", width: "1200px" }}
+        className="flex flex-col text-left justify-between pt-8 relative"
+      >
+        <div>
+          <SectionTitle title="Real-World Projects." />
+        </div>
+        <div className="grid grid-cols-1 gap-12 md:gap-5 md:grid-cols-3 items-start mt-5">
+          <Card
+            image="ekrili.png"
+            title="Ekrili"
+            desc="Rental website for homes and villas: Find, book, and stay."
+            techs={["NextJs", "ExpressJs", "Material UI", "MongoDB"]}
+            link="https://ekrili.dz"
+            github=""
+          />
 
-            <Card 
+          <Card
             image="aposto.png"
             title="Aposto store"
-            desc="E-commerce website for Aposto : Clothes brand." 
-            techs={["NextJs" , "Typescript" , "Bootstrap" , "Prisma"]}
-            link="https://www.aposto.store"  
+            desc="E-commerce website for Aposto : Clothes brand."
+            techs={["NextJs", "Typescript", "Bootstrap", "Prisma"]}
+            link="https://www.aposto.store"
             github="https://github.com/Rayane76/aposto"
-            />
+          />
 
-            <Card 
+          <Card
             image="candy.png"
             title="Candy lokum"
-            desc="E-commerce website for candies." 
-            techs={["NextJs" , "Bootstrap" , "Mongodb" , "Docker"]}
-            link="http://candy-lokum.ai2dz.com/"  
+            desc="E-commerce website for candies."
+            techs={["NextJs", "Bootstrap", "Mongodb", "Docker"]}
+            link="http://candy-lokum.ai2dz.com/"
             github=""
-            />
+          />
 
-            <Card 
+          <Card
             image="ghack.png"
             title="Ghack"
-            desc="Google Developers Group of Algiers hackathon website." 
-            techs={["NextJs" , "TailwindCss"]}
-            link="https://ghack24.gdgalgiers.com/"  
+            desc="Google Developers Group of Algiers hackathon website."
+            techs={["NextJs", "TailwindCss"]}
+            link="https://ghack24.gdgalgiers.com/"
             github=""
-            />
+          />
 
-{/* <Card 
+          {/* <Card 
             image="takwim.png"
             title="Takwim"
             desc="Website for islamic scholar Mshmsdin." 
@@ -45,22 +56,17 @@ function Projects() {
             link="#"  
             github=""
             /> */}
-
-
-      </div>
-      {/* <div className="relative w-full mt-2">
+        </div>
+        {/* <div className="relative w-full mt-2">
         <a href="/projects">
           <div className="mt-8 max-w-sm md:max-w-2xl border border-fun-pink mx-auto text-center w-full whitespace-nowrap px-8 py-3 rounded-full text-fun-pink bg-fun-pink-darkerer hover:bg-fun-pink hover:text-white transition-colors cursor-pointer">
             View All
           </div>
         </a>
       </div> */}
-    </div>
+      </div>
     </div>
   );
 }
 
 export default Projects;
-
-
-
--- src/app/components/home/work/Work.jsx
+++ src/app/components/home/work/Work.jsx
@@ -1,72 +1,133 @@
-'use client'
+"use client";
 import SectionTitle from "../../SectionTitle";
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
 import { useState } from "react";
 
+export default function Work() {
+  const [value, setValue] = useState(0);
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
+  function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
 
-export default function Work(){
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      </div>
+    );
+  }
 
-    const [value,setValue] = useState(0);
+  return (
+    <div className="w-full flex items-center flex-col justify-center">
+      <div style={{ width: "1200px", maxWidth: "1200px" }} className="mt-10">
+        <SectionTitle title="Where i've worked. " />
+      </div>
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-      };
-
-      function CustomTabPanel(props) {
-        const { children, value, index, ...other } = props;
-      
-        return (
-          <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-          >
-            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-          </div>
-        );
-      }
-
-    return(
-        <div className="w-full flex items-center flex-col justify-center">
-            <div style={{width:"1200px",maxWidth:"1200px"}} className="mt-10">
-               <SectionTitle title="Where i've worked. " />
+      <div className="mt-10 mb-10">
+        <Box sx={{ width: "100%", display: "flex" }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              orientation="vertical"
+              value={value}
+              onChange={handleChange}
+              aria-label="basic tabs example"
+            >
+              <Tab label="CodeItDz" />
+              <Tab label="Ekrili" />
+              {/* <Tab label="Item Three"  /> */}
+            </Tabs>
+          </Box>
+          <CustomTabPanel style={{ color: "white" }} value={value} index={0}>
+            <div>
+              <h1 className="text-2xl">
+                Web Developer{" "}
+                <a
+                  href="https://codeitdz.com/"
+                  target="_blank"
+                  className="text-fun-pink"
+                >
+                  @CodeItDz
+                </a>
+              </h1>
+              <p className="text-sm text-fun-gray mt-2">
+                February 2024 - August 2024
+              </p>
+              <ul className="mt-2">
+                <li className="workList text-base mb-2">
+                  Implemented UI components with React & Next.js, Bootstrap &
+                  Tailwind CSS.
+                </li>
+                <li className="workList text-base mb-2">
+                  Understanding client needs and proposing effective solutions,
+                  which also involves strategising and planning.
+                </li>
+                <li className="workList text-base mb-2">
+                  Developed robust backend APIs using Next.js, MongoDB, and
+                  Express.js, ensuring seamless data flow and high performance.
+                </li>
+                <li className="workList text-base mb-2">
+                  Hosted multiple projects on a personal server using Docker,
+                  ensuring efficient deployment and consistent environments.
+                </li>
+              </ul>
             </div>
-
-            <div className="mt-10 mb-10">
-            <Box sx={{ width: '100%' , display: "flex" }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs  orientation="vertical" value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="CodeItDz" />
-          {/* <Tab label="Item Two" />
-          <Tab label="Item Three"  /> */}
-        </Tabs>
-      </Box>
-      <CustomTabPanel style={{color:"white"}} value={value} index={0}>
-        <div>
-            <h1 className="text-2xl">Web Developer <a href="https://codeitdz.com/" target="_blank" className="text-fun-pink">@CodeItDz</a></h1>
-            <p className="text-sm text-fun-gray mt-2">February 2024 - August 2024</p>
-            <ul className="mt-2">
-                <li className="workList text-base mb-2">Implemented UI components with React & Next.js, Bootstrap & Tailwind CSS.</li>
-                <li className="workList text-base mb-2">Understanding client needs and proposing effective solutions, which also involves strategising and planning.</li>
-                <li className="workList text-base mb-2">Developed robust backend APIs using Next.js, MongoDB, and Express.js, ensuring seamless data flow and high performance.</li>
-                <li className="workList text-base mb-2">Hosted multiple projects on a personal server using Docker, ensuring efficient deployment and consistent environments.</li>
-            </ul>
-        </div>
-      </CustomTabPanel>
-      {/* <CustomTabPanel style={{color:"white"}} value={value} index={1}>
-        Item Two
-      </CustomTabPanel>
-      <CustomTabPanel style={{color:"white"}} value={value} index={2}>
+          </CustomTabPanel>
+          <CustomTabPanel style={{ color: "white" }} value={value} index={1}>
+            <div>
+              <h1 className="text-2xl">
+                Full Stack Engineer{" "}
+                <a
+                  href="https://ekrili.dz"
+                  target="_blank"
+                  className="text-fun-pink"
+                >
+                  @Ekrili
+                </a>
+              </h1>
+              <p className="text-sm text-fun-gray mt-2">
+                January 2025 - June 2025
+              </p>
+              <ul className="mt-2">
+                <li className="workList text-base mb-2">
+                  Built the Ekrili v2 platform from scratch using Next.js,
+                  Tailwind CSS, and Material UI for Frontend, Express.js with
+                  MongoDB for Backend.
+                </li>
+                <li className="workList text-base mb-2">
+                  Implemented efficient data fetching using TanStack Query,
+                  Integrated FCM for real-time push notifications.
+                </li>
+                <li className="workList text-base mb-2">
+                  Designed and executed complex MongoDB aggregation pipelines
+                  for data transformation.
+                </li>
+                <li className="workList text-base mb-2">
+                  Performed production database migrations for over 1,000
+                  records.
+                </li>
+                <li className="workList text-base mb-2">
+                  Configured and maintained KrakenD API Gateway, Containerized
+                  the application using Docker
+                </li>
+              </ul>
+            </div>
+          </CustomTabPanel>
+          {/* <CustomTabPanel style={{color:"white"}} value={value} index={2}>
         Item Three
       </CustomTabPanel> */}
-    </Box>
-            </div>
-        </div>
-    )
+        </Box>
+      </div>
+    </div>
+  );
 }
